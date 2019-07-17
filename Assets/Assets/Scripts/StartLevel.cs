a47fe08e1e33dbd10cd10a38e25f1b24d7d4aa4e
Enderlinq <wahj060@gmail.com>
2019-07-17 22:27:54 +0300

    MENU SCREEN AND LOADING SCRENE

---
+++ Assets/Assets/Scripts/StartLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadFirstScene();
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
+
